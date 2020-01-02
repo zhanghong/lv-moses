8bb20f232c185c91dec5376a255f07be137a9e6e
zhanghongbean <zhanghong_bean@126.com>
2020-01-02 18:13:06 +0800

    添加ShopBrand资源类

--- app/Http/Controllers/Shop/BrandController.php
+++ app/Http/Controllers/Shop/BrandController.php
@@ -7,19 +7,21 @@ use App\Models\Shop\Shop;
 use App\Models\Shop\Brand;
 use Illuminate\Http\Request;
 use App\Http\Requests\Shop\BrandRequest;
+use App\Http\Resources\Shop\BrandResource;
 
 class BrandController extends Controller
 {
     public function index(Shop $shop)
     {
-        return $shop->brands()->paginate();
+        $paginate = $shop->brands()->paginate();
+        return BrandResource::collection($paginate);
     }
 
     public function store(BrandRequest $request, Shop $shop)
     {
         $brand = new Brand($request->all());
         $shop->brands()->save($brand);
-        return $brand;
+        return new BrandResource($brand);
     }
 
     public function show(Shop $shop, Brand $brand)
@@ -27,7 +29,7 @@ class BrandController extends Controller
         if ($shop->id != $brand->shop_id) {
             return [];
         }
-        return $brand;
+        return new BrandResource($brand);
     }
 
     public function update(BrandRequest $request, Shop $shop, Brand $brand)
@@ -38,12 +40,12 @@ class BrandController extends Controller
 
         $brand->fill($request->all());
         $brand->save();
-        return $brand;
+        return new BrandResource($brand);
     }
 
     public function destroy(Shop $shop, Brand $brand)
     {
         $brand->delete();
-        return [];
+        return response()->json(null, 204);
     }
 }
---
+++ app/Http/Resources/Shop/BrandResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Shop;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BrandResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'logo_url' => $this->logo_url,
+            'description' => $this->description,
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
