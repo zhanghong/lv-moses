788ab9a96b7214b7b9b9238a9efc4e3a49f19875
zhanghongbean <zhanghong_bean@126.com>
2020-02-03 17:25:17 +0800

    更新物流公司同步方式

---
+++ database/migrations/2020_02_03_155523_add_outer_columns_to_expresses.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOuterColumnsToExpresses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('expresses', function (Blueprint $table) {
+            $table->string('outer_name', 6)->default('')->comment('来源名称');
+            $table->string('outer_key', 10)->default('')->comment('来源主键');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('expresses', function (Blueprint $table) {
+            $table->dropColumn('outer_name');
+            $table->dropColumn('outer_key');
+        });
+    }
+}
--- database/seeds/BaseExpressSeeder.php
+++ database/seeds/BaseExpressSeeder.php
@@ -13,17 +13,23 @@ class BaseExpressSeeder extends Seeder
     public function run()
     {
         $list = [
-            ['code' => 'shentong', 'name' => '申通快递'],
-            ['code' => 'yunda', 'name' => '韵达速递'],
-            ['code' => 'zhongtong', 'name' => '中通快递'],
-            ['code' => 'shunfeng', 'name' => '顺丰速运'],
-            ['code' => 'ems', 'name' => 'EMS'],
+            ['code' => 'ems', 'name' => 'EMS', 'outer_key' => '2000'],
+            ['code' => 'jingdong', 'name' => '京东物流', 'outer_key' => '2017'],
+            ['code' => 'shunfeng', 'name' => '顺丰速运', 'outer_key' => '2011'],
+            ['code' => 'yunda', 'name' => '韵达', 'outer_key' => '2005'],
+            ['code' => 'dhl', 'name' => 'DHL', 'outer_key' => '2002'],
+            ['code' => 'yuantong', 'name' => '圆通', 'outer_key' => '2001'],
+            ['code' => 'zhongtong', 'name' => '中通', 'outer_key' => '2004'],
+            ['code' => 'huitongkuaidi', 'name' => '百世快递', 'outer_key' => '2008'],
+            ['code' => 'debangwuliu', 'name' => '德邦', 'outer_key' => '2009'],
+            ['code' => 'shentong', 'name' => '申通快递', 'outer_key' => '2010'],
+            ['code' => 'shunxing', 'name' => '顺兴', 'outer_key' => '2012'],
+            ['code' => 'rufeng', 'name' => '如风达', 'outer_key' => '2014'],
+            ['code' => 'yousu', 'name' => '优速', 'outer_key' => '2015'],
+            ['code' => 'changling', 'name' => '畅灵', 'outer_key' => '2006'],
             ['code' => 'zhaijisong', 'name' => '宅急送'],
-            ['code' => 'quanfengkuaidi', 'name' => '全峰快递'],
-            ['code' => 'huitongkuaidi', 'name' => '百世快递'],
+            ['code' => 'quanfeng', 'name' => '全峰快递'],
             ['code' => 'tiantian', 'name' => '天天快递'],
-            ['code' => 'youshuwuliu', 'name' => '优速物流'],
-            ['code' => 'debangwuliu', 'name' => '德邦快递'],
         ];
         foreach ($list as $key => $item) {
             $express = Express::firstOrNew(['code' => $item['code']]);
@@ -31,6 +37,15 @@ class BaseExpressSeeder extends Seeder
                 // 新建的分类默认是可见的
                 $express->is_enabled = true;
             }
+            if (isset($item['outer_key'])) {
+                $express->outer_name = Express::OUTER_FROM_WECHAT;
+                $express->outer_key = $item['outer_key'];
+            } else {
+                $express->outer_name = '';
+                $express->outer_key = '';
+            }
+            $express->name = $item['name'];
+            $express->order = $key + 1;
             $express->save();
         }
     }
